518e73ac704046338aa39429f810cad9a2f1a368
tuannx21 <xuan.tuan@1pac.vn>
2018-12-11 14:44:52 +0700

    update section intro

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 window.addEventListener('DOMContentLoaded', function () {
   console.log('loaded');
+
+  var toggleShowMore = Array.prototype.slice.call(document.querySelectorAll(".card-introduce .content .body-sp"));
+  toggleShowMore.forEach(function(tg) {
+    tg.addEventListener('click',function() {
+      let targetArrow = document.querySelector("#"+tg.getAttribute("arrow-target"));
+      let targetContent = document.querySelector("#"+tg.getAttribute("content-target"));
+      
+      if (targetContent.style.display==="block") {
+        targetContent.style.display="none";
+        targetArrow.src="images/icon/arrow_open.png";
+      } else {
+        targetContent.style.display="block";
+        targetArrow.src="images/icon/arrow_close.png";
+      }
+    });
+  });
 })
